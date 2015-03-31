ef4878260662d97c2b2a6ad66e87234fe94dfc05
Hal Henke <halhenke@gmail.com>
2015-03-31 18:47:48 +1100

    Profile Stuff

---
+++ routes/api/profiles.js
@@ -0,0 +1,40 @@
+var keystone = require('keystone'),
+    async = require('async');
+
+exports = module.exports = function(req, res) {
+
+  var view = new keystone.View(req, res),
+    locals = res.locals;
+
+  // Init locals
+  locals.section = 'profiles';
+  locals.filters = {
+    category: req.params.category
+  };
+  locals.data = {
+    profiles: [],
+    categories: []
+  };
+
+  // Load the profiles
+  view.on('init', function(next) {
+
+    var q = keystone.list('Profile').paginate({
+        page: req.query.page || 1,
+        perPage: 10,
+        maxPages: 10
+      })
+      .or([{'state': 'public'}, {'owner': locals.user.id}])
+      // .sort('-publishedDate')
+      .populate('owner dolls');
+
+    q.exec(function(err, results) {
+      locals.data.profiles = results;
+      next(err);
+    });
+
+  });
+
+  // Render the view
+  view.render('profiles');
+};
