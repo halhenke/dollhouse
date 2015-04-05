924ecbf33ff99b7dc83e1b0f2df20a23dc76ac01
Hal Henke <halhenke@gmail.com>
2015-04-05 18:51:07 +1000

    9 dolls per page

--- routes/api/dolls.js
+++ routes/api/dolls.js
@@ -21,7 +21,7 @@ exports = module.exports = function(req, res) {
 
     var q = keystone.list('Doll').paginate({
         page: req.query.page || 1,
-        perPage: 10,
+        perPage: 9,
         maxPages: 10
       })
       // NOTE: Stop page breaking for people who arent loggged in
