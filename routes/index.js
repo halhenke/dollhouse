9dc4f5624f4cb717972c33c3651cb22b39ffc04d
Hal Henke <halhenke@gmail.com>
2015-04-07 02:52:18 +1000

    De-paginate API calls and routes from slush generator

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,13 @@ exports = module.exports = function(app) {
   app.get('/api/profiles', routes.api.profiles);
   app.get('/profiles/show/:profile', routes.views.profileView);
   app.get('/users/show/:profile', routes.views.profileView);
+
+  // app.get('/profiles', routes.views.indexProfiles)
   app.all('/profiles/new', routes.views.newProfile)
+  // app.get('/profiles/profile:profile', routes.views.showProfile)
+  // app.get('/profiles/profile/:profile/edit', routes.views.editProfile)
+  // app.get('/profiles/profile/:profile/delete', routes.views.deleteProfile)
+
   app.all('/dolls/new', routes.views.newDoll);
   app.get('/about', routes.views.about);
   app.all('/contact', routes.views.contact);
