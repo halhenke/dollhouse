66d113f6a5940049365ba19a879478e6e48293f5
Hal Henke <halhenke@gmail.com>
2015-05-24 08:02:22 +1000

    Passport login/register controllers and views

---
+++ routes/passport/login.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose"),
+  // passport = require('passport'),
+  keystone = require('keystone'),
+  User = keystone.list('User');
+  // LocalStrategy = require('passport-local').Strategy;
+
+// var uri = "mongodb://localhost:27017/dollhouse";
+// var db = mongoose.createConnection(uri);
+
+
+module.exports = function (req, res) {
+  var view = new keystone.View(req, res);
+
+  view.render('passport/login');
+};
