93c9c244c6fc5295a9a4d3057d91dafb4ec6f83d
Hal Henke <halhenke@gmail.com>
2015-05-26 23:44:17 +1000

    Fix showProfile route after schema change (embed Profile in User)

--- routes/api/showProfile.js
+++ routes/api/showProfile.js
@@ -32,27 +32,27 @@ exports = module.exports = function(req, res) {
         locals.data.profile = profile;
         return keystone.list('Doll').model
           .find()
-          .where({owner: profile })
+          .where({owner: profile._id })
           .exec();
       }, function (err) {
         console.log('We got err ' + err);
       })
       // Handle Dolls
       .then(function (dolls) {
-        console.log('Dolls found for ' + locals.data.profile.user.name + " are " + dolls);
+        console.log('Dolls found for ' + locals.data.profile.name + " are " + dolls);
         if (dolls.length > 0) {
           console.log('showProfile API call - We found some dolls ' + dolls.length);
           locals.data.dolls = dolls;
         }
         return keystone.list('CommunityLink').model
           .find()
-          .where({ owner: locals.data.profile })
+          .where({ owner: locals.data.profile._id })
           .exec();
       }, function (err) {
         console.log('We got err ' + err);
       })
       .then(function (links) {
-        console.log('Links found for ' + locals.data.profile.user.name + " are " + links);
+        console.log('Links found for ' + locals.data.profile.name + " are " + links);
         // Handle Links
         if (links.length > 0) {
           locals.data.links = links;
@@ -63,7 +63,7 @@ exports = module.exports = function(req, res) {
             // _id: { $ne:  }
             "location.geo":
               { $near: { $geometry:
-                  { type: "Point", coordinates: locals.data.profile.user.location.geo },
+                  { type: "Point", coordinates: locals.data.profile.location.geo },
                     $maxDistance: 20 } }
           // }, "_id")
           })
@@ -72,7 +72,7 @@ exports = module.exports = function(req, res) {
         console.log('We got err ' + err);
       })
       .then(function (neighbours) {
-        console.log('Profile Neighbours found for ' + locals.data.profile.user.name + " are " + neighbours);
+        console.log('Profile Neighbours found for ' + locals.data.profile.name + " are " + neighbours);
         locals.data.neighbours = neighbours;
         next();
       });
