4cc58baf2c7c61032a5df78fa63a48727e34149d
Hal Henke <halhenke@gmail.com>
2015-07-20 21:11:43 +1000

    set eventDetails in controller

--- routes/views/event.js
+++ routes/views/event.js
@@ -24,6 +24,23 @@ exports = module.exports = function(req, res) {
 
     q.exec(function(err, result) {
       locals.data.event = result;
+      locals.data.eventDetails = [
+        {
+          key: "Date",
+          val: locals.data.event._.startDate.format("dddd, MMMM Do") +
+                " - " +
+                locals.data.event._.endDate.format("dddd, MMMM Do")
+        }, {
+          key: "Time",
+          val: locals.data.event._.startTime.format("hh:mm a") +
+                " - " +
+                locals.data.event._.endTime.format("hh:mm a")
+        }, {
+          key: "Details",
+          val: locals.data.event.placeString
+        }
+      ];
+
       next(err);
     });
 
@@ -32,7 +49,12 @@ exports = module.exports = function(req, res) {
   // Load other events
   view.on('init', function(next) {
 
-    var q = keystone.list('Event').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('4');
+    var q = keystone.list('Event').model
+      .find()
+      .where('state', 'published')
+      .sort('-publishedDate')
+      .populate('author')
+      .limit('4');
 
     q.exec(function(err, results) {
       locals.data.events = results;
